fb6e59613c0e097b88228af74c4c0a0e8edfca6b
Toby Hughes <tobywhughes@gmail.com>
2021-02-27 19:55:54 -0800

    Parses filename into metadata struct

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 mod configuration;
 mod environment;
+mod file;
 
 use configuration::init::InitializationConfiguration;
 use environment::cli::parse_command_line_arguments;
+use file::parse::FileMetadata;
 
 fn main() {
     let mut initialization_configuration: InitializationConfiguration = Default::default();
 
     parse_command_line_arguments(&mut initialization_configuration);
 
-    println!("{:?}", initialization_configuration)
+    let file_metadata = FileMetadata::new(&initialization_configuration.filename);
+
+    println!("{:?}", file_metadata)
 }
