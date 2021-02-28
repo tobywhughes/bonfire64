c001fce233c795a2da6829aaec6569c1f20ce8e6
Toby Hughes <tobywhughes@gmail.com>
2021-02-28 02:18:14 -0800

    Added logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#[macro_use]
+extern crate log;
+
+use env_logger::Env;
+
 mod configuration;
 mod environment;
 mod file;
@@ -7,11 +12,17 @@ use environment::cli::parse_command_line_arguments;
 use file::read::BonfireFile;
 
 fn main() {
+    initialize_logger();
     let mut initialization_configuration: InitializationConfiguration = Default::default();
 
     parse_command_line_arguments(&mut initialization_configuration);
 
     let bonfire_file: BonfireFile = BonfireFile::new(&initialization_configuration);
 
-    println!("{:?}", bonfire_file)
+    info!("{:?}", bonfire_file);
+}
+
+fn initialize_logger() -> () {
+    let log_env = Env::default().filter_or("BONFIRE_LOG_LEVEL", "info");
+    env_logger::init_from_env(log_env);
 }
